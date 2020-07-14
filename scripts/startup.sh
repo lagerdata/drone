f341fa6f9699845b9208aa908104020baae5901f
Evan Haas <evan@lagerdata.com>
2020-07-14 15:11:04 -0700

    include explicit lager host header in registration callback

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -q --header "Authorization: Secret $LAGER_AUTH_TOKEN" -O - $LAGER_CONTAINER_REGISTRATION_URL
+wget -q --header "Authorization: Secret $LAGER_AUTH_TOKEN" --header "Host: $LAGER_HOST" -O - $LAGER_CONTAINER_REGISTRATION_URL
 exec /bin/drone-server
