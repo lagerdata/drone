a455cf70c3a389a97406ed787625541aabdf0ccb
Evan Haas <evan@lagerdata.com>
2020-07-14 16:28:53 -0700

    don't start drone if container registration fails

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-wget -q --header "Authorization: Secret $LAGER_AUTH_TOKEN" --header "Host: $LAGER_HOST" -O - $LAGER_CONTAINER_REGISTRATION_URL
-exec /bin/drone-server
+wget -q --header "Authorization: Secret $LAGER_AUTH_TOKEN" --header "Host: $LAGER_HOST" -O - $LAGER_CONTAINER_REGISTRATION_URL 2&>/dev/null > /dev/null && exec /bin/drone-server
